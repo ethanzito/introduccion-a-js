05cebf17d8495d937b032db818ec330d04858dd3
ethanzito <Tommys.Worker@gmail.com>
2023-12-18 00:16:00 -0300

    2) Mostrar bienvenida con los datos del usuario al presionar Continuar

---
+++ tareas/clase-5/tarea-clase-5-b.js
@@ -0,0 +1,18 @@
+// 2)
+
+document.querySelector('#datos-usuario').onclick = function() {
+  const nombre = document.querySelector('#nombre-usuario').value;
+  const apellido = document.querySelector('#apellido-usuario').value;
+  const edad = document.querySelector('#edad-usuario').value;
+
+  document.querySelector('#msj-bienvenido').innerText = `Bienvenido ${nombre} ${apellido}! (${edad} años)`
+
+  return false;
+}
+
+//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
